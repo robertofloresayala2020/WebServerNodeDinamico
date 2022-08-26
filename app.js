72e1d6ae20b399f1bcb139554eeb773807f599e4
Roberto Flores <roberto.flores.ayala@gmail.com>
2022-08-26 12:11:14 -0500

    WebServerDinamico

--- app.js
+++ app.js
@@ -7,33 +7,52 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import hbs from 'handlebars'
+import 'dotenv/config' 
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
-console.log('iniciando');
-const port=8080;
-
+const port=process.env.PORT;
 const app= express();
 
+
+
+
+
+hbs.registerPartial(__dirname + '/views/partials',function(err){});
+app.set('view engine', 'hbs'); 
 
 
 //Servir contenido estático
 app.use(express.static('public'));
 
+app.get('/',(req,res)=>
+{
+    
+    
+    // res.send('hola'); //Renderiza el string
+    res.render('home',{             //Renderiza las vista home.hbs
+        nombre:'Roberto Flores',    //Envia las propiedades
+        titulo:'Curso de Node'
 
-app.get('/generic',(req,res)=>
+    }); //van los argumentos 
+
+
+})
+
+/*
+app.get('/generics',(req,res)=>
 {
     res.sendFile(__dirname +'/public/generic.html');
 
 })
 
-
 app.get('/elements',(req,res)=>
 {
     res.sendFile(__dirname +'/public/elements.html');
 
-})
+})*/
 app.get('*',(req,res)=>
 {
     res.sendFile (__dirname + '/public/404.html');
